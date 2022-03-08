723b9296b330e37b93cfa4c58c8e3e680fd49e03
vijaysaravanan1812 <vijaysaravanan1812@gmail.com>
2022-03-08 22:57:24 +0530

    -

---
+++ String/Week 1/problem06.cpp
@@ -0,0 +1,27 @@
+
+
+/*
+Given two strings a and b. The task is to find if the string 'b' 
+can be obtained by rotating another string 'a' by exactly 2 places.
+*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool rotate(string a, string b)
+{
+    a.copy();
+
+
+
+}
+
+
+int main()
+{
+    string a = "amazon";
+    string b = "azonam"; 
+
+}
